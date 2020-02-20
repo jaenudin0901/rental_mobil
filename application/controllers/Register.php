b656e6b5cd7e85d74e71ab80d05ab26cdbe9e1f5
jaenudin0901 <jaenudinj913198@gmail.com>
2020-02-20 19:04:22 +0700

    menambahkan file register.php pada Con & file register_form.php pada view

---
+++ application/controllers/Register.php
@@ -0,0 +1,55 @@
+<?php 
+
+class Register extends CI_Controller {
+
+	public function index()
+	{	
+		$this->_rules();
+
+		if($this->form_validation->run() == FALSE){
+
+		$this->load->view('templates_admin/header');
+		$this->load->view('register_form');
+		$this->load->view('templates_admin/footer');
+		} else {
+				$nama  = $this->input->post('nama');
+			$username 		= $this->input->post('username');
+			$password 	= $this->input->post('password');
+			$alamat 	= $this->input->post('alamat');
+			$gender 		= $this->input->post('gender');
+			$no_telp 	= $this->input->post('no_telp');
+			$no_ktp 	= $this->input->post('no_ktp');
+		
+	
+		$data = array(
+				'nama' =>	$nama,
+				'username'		=>	$username,
+				'password'	=>	$password,
+				'alamat'		=> 	$alamat,
+				'gender'		=> 	$gender,
+				'no_telp'	=>  $no_telp,
+				'no_ktp'	=>	$no_ktp
+			);
+
+		$this->Customer_model->insert_data($data, 'customer');
+		$this->session->set_flashdata('pesan','   <div class="alert alert-primary alert-dismissble fade show" role="alert"> Berhasil register, Silahkan Login!.
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+          </button>
+     </div>');
+		redirect('auth/login');
+		
+		}
+	}
+
+	public function _rules()
+	{
+		$this->form_validation->set_rules('nama', 'nama', 'required');
+		$this->form_validation->set_rules('username', 'username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('alamat', 'Alamat', 'required');
+		$this->form_validation->set_rules('gender', 'Gender', 'required');
+		$this->form_validation->set_rules('no_telp', 'no_telp', 'required');
+		$this->form_validation->set_rules('no_ktp', 'No Ktp', 'required');
+	}
+}
--- application/views/templates_customer/header.php
+++ application/views/templates_customer/header.php
@@ -41,7 +41,7 @@
             <a class="nav-link" href="#">Services</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="<?= base_url('register'); ?>">Register</a>
           </li>
         </ul>
       </div>
